729c866cac5f25b10a8b5dc2fffdf8e939a90afa
trungtuan2003 <tuntrn777@gmail.com>
2024-01-02 21:05:14 +0700

    add function showSuccessToast(), showWarningToast()

---
+++ index.js
@@ -0,0 +1,61 @@
+function toast ({
+    title = '', 
+    message = '',
+    type = 'info',   
+    duration = 3000
+}) {
+    const main = document.getElementById("toast");
+    
+    if(main) {
+        const icons = {
+            success: 'fa-solid fa-circle-check',
+            info : 'fa-solid fa-circle-info',
+            warning: 'fa-solid fa-circle-exclamation'
+        }
+
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+
+        const toast = document.createElement("div");
+        toast.classList.add("toast", `toast--${type}`);
+        toast.style.animation = `slideIn ease .3s, fadeOut linear 1s ${delay}s forwards`;
+        toast.innerHTML = `
+            <div class="toast__icon">
+                    <i class="${icon}"></i>
+                </div>
+                <div class="toast__body">
+                    <div class="toast__title">
+                        ${title}
+                    </div>
+                    <div class="toast__msg">
+                        ${message}
+                    </div>
+                </div> 
+                <div class="toast__close",  onclick="removeToast();">
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+        `;
+        main.appendChild(toast);
+    }
+}
+
+function removeToast() {
+    toast.remove();
+}
+function showSuccessToast() {
+    toast({
+        title: 'Success',
+        message: 'Thanh cong',
+        type: 'success',
+        duration: 3000
+    });
+} 
+
+function showWarningToast() {
+    toast({
+        title: 'Warning',
+        message: 'That bai',
+        type: 'warning',
+        duration: 3000
+    });
+}
